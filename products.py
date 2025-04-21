1a601f9ffb813e85eb8eb3e2db28531221c4ab70
JflPSU25 <jfl6172@psu.edu>
2025-04-20 23:41:53 -0400

    added productregistrar page, worked on displaying products (running into wall on database access)

--- products.py
+++ products.py
@@ -5,3 +5,7 @@ products_bp = Blueprint('products', __name__, template_folder='templates')
 @products_bp.route('/MyProducts', methods=['POST', 'GET'])
 def loadSellerProducts():
     return render_template('sellerproducts.html')
+
+@products_bp.route('/ProductRegistration', methods=['POST', 'GET'])
+def loadProductRegistrar():
+    return render_template('productregisterform.html')
