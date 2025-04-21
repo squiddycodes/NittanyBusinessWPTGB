294ac1c7e7f41dacb8a5b18f191ec55d58195a48
JflPSU25 <jfl6172@psu.edu>
2025-04-21 15:11:46 -0400

    displayed products on seller products page, added buttons to move forward and back. moving to add new products page

--- products.py
+++ products.py
@@ -1,11 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import pandas as pd
+import sqlite3 as sql
 
 products_bp = Blueprint('products', __name__, template_folder='templates')
 
 @products_bp.route('/MyProducts', methods=['POST', 'GET'])
 def loadSellerProducts():
-    return render_template('sellerproducts.html')
+    email = request.form['email']
+
+    connection = sql.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT Product_Title, Listing_ID, Category, Quantity, Status FROM Product_Listings WHERE Seller_Email = ?', (email,))
+    products = cursor.fetchall()
+    connection.close()
+
+    return render_template('sellerproducts.html', email=email, productfile = products)
 
 @products_bp.route('/ProductRegistration', methods=['POST', 'GET'])
 def loadProductRegistrar():
     return render_template('productregisterform.html')
+
+@products_bp.route('/HomePage', methods=['POST', 'GET'])
+def returntoHomePage():
+    email = request.form['email']
+    return render_template('sellersLandingPage.html', email = email)
