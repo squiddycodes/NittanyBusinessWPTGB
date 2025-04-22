14ecd004ab164d52f2a12ce0f311b04a36963dfb
squiddycodes <dguidorizzi@yahoo.com>
2025-04-21 23:51:52 -0400

    product pages linked for BrowseProducts

--- BrowseProducts.py
+++ BrowseProducts.py
@@ -20,7 +20,19 @@ def loadProductsPage():
 
     return render_template('productcatalogue.html', email=email, productfile=products)
 
-@BrowseProducts_bp.route('/SellerHomePage', methods=['POST', 'GET'])
+@BrowseProducts_bp.route('/BuyersHomePage', methods=['POST', 'GET'])
 def returntoHomePage():
     email = request.form['email']
-    return render_template('sellersLandingPage.html', email = email)
+    return render_template('buyersLandingPage.html', email = email)
+
+@BrowseProducts_bp.route("/BrowseProducts/<int:listing_id>")
+def browse_product(listing_id):
+    print(listing_id)
+    connection = sql.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute(
+        'SELECT Product_Title, Listing_ID, Category, Quantity, Status FROM Product_Listings WHERE Listing_ID = ?', (listing_id,))
+    product = cursor.fetchone()
+    connection.close()
+    print("product", product)
+    return render_template("productPage.html", product=product)
