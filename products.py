150ccc5eb7c2c4fbd0f079da7d483e396e468b67
JflPSU25 <jfl6172@psu.edu>
2025-04-23 22:33:23 -0400

    finished editproduct, moving to new product

--- products.py
+++ products.py
@@ -26,8 +26,8 @@ def returntoHomePage():
     email = request.form['email']
     return render_template('sellersLandingPage.html', email = email)
 
-@products_bp.route("/MyProducts/EditProduct/<int:listing_id>", methods=['POST', 'GET'])
-def edit_product(listing_id):
+@products_bp.route("/MyProducts/EditProduct/<string:email>Editing<int:listing_id>", methods=['POST', 'GET'])
+def edit_product(email, listing_id):
     print("Hello World")
     #email = request.form['email']
     print(listing_id)
@@ -44,18 +44,43 @@ def edit_product(listing_id):
     product = cursor.fetchone()
     connection.close()
     print("product", product)
-    return render_template("editproduct.html", product=product, listing_id=listing_id)
+    return render_template("editproduct.html", product=product, listing_id=listing_id, email = email)
 
 @products_bp.route("/UpdateProduct", methods = ['POST', 'GET'])
 def update_product():
     listing_id = request.form['listing_id']
+    newProdTitle = request.form['newProdTitle']
+    newProdName = request.form['newProdName']
+    newProdDescription = request.form['newProdDescription']
+    newProdCategory = request.form['newProdCategory']
+    newProdQuantity = request.form['newProdQuantity']
+    newProdPrice = request.form['newProdPrice']
+    newProdStatus = request.form['newProdStatus']
+    email = request.form['email']
     print("listing_id to be updated: ", listing_id)
+    print("email", email)
     connection = sql.connect('database.db')
+
     cursor = connection.cursor()
 
     cursor.execute(
-        'UPDATE Product_Listings SET Product_Title = newProdName,'
-        'Quantity = newProdQty, '
-        'Status = newProdStatus WHERE Listing_ID = ?', (listing_id,)
+        'UPDATE Product_Listings SET Product_Title = ?,'
+        'Product_Name = ?,'
+        'Product_Description = ?,'
+        'Category = ?,'
+        'Quantity = ?,'
+        'Product_Price = ?,'
+        'Status = ? WHERE Listing_ID = ?', (newProdTitle,newProdName,newProdDescription,newProdCategory,newProdQuantity,newProdPrice,newProdStatus,listing_id,)
     )
+    connection.commit()
     connection.close()
+
+    connection = sql.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute(
+        'SELECT Product_Title, Listing_ID, Category, Quantity, Status FROM Product_Listings WHERE Seller_Email = ?',
+        (email,))
+    products = cursor.fetchall()
+    connection.close()
+
+    return render_template('sellerproducts.html', email = request.form['email'], productfile = products)
