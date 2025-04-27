a30adeb842d12f9c3eacdca8f4dc77753b2508d4
nic-che <nichen2939@gmail.com>
2025-04-26 23:54:39 -0400

    testing review modal on create account page

--- reviews.py
+++ reviews.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 reviews_bp = Blueprint('reviews', __name__, template_folder='templates')
 
-@reviews_bp.route("/reviews")
+@reviews_bp.route("/MakeReview", methods=['POST', 'GET'])
 def reviews():
-    return 'Hello, World!'
+    return render_template('input.html', orderSubmitted=True)
