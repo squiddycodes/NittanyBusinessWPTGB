9f9a2be64c2101d2b4ab0390396db67d37e06546
bmalla21 <157842085+bmalla21@users.noreply.github.com>
2025-04-27 12:46:58 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,56 +1,69 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 import sqlite3 as sql
-from werkzeug.security import generate_password_hash
 
-editprofile_bp = Blueprint("editprofile", __name__, template_folder="templates")
+# Define the Blueprint for profile-related routes
+profile_bp = Blueprint('profile_bp', __name__, template_folder="templates"))
 
-@editprofile_bp.route("/EditProfileInformation", methods=["GET", "POST"])
-def edit_profile():
-    if request.method == "GET":
-        email = request.args.get("email")
-        if not email:
-            return redirect(url_for("login"))  # Redirect if email not provided
+# Route to view the profile (this will show different pages based on user type)
+@profile_bp.route('/profile')
+def view_profile():
+    email = request.args.get('email')  # Get the email of the logged-in user
 
-        conn = sql.connect("database.db")
-        cur = conn.cursor()
-        cur.execute("SELECT BusinessName, Street, City, State, Zipcode FROM Users WHERE Email = ?", (email,))
-        user_data = cur.fetchone()
-        conn.close()
-
-        return render_template("profile.html", email=email, user=user_data)
-
-    # POST request: user is submitting updated form
-    email = request.form.get("email")
-    if not email:
-        return redirect(url_for("login"))
-
-    new_business_name = request.form.get("business_name")
-    new_street = request.form.get("street")
-    new_city = request.form.get("city")
-    new_state = request.form.get("state")
-    new_zipcode = request.form.get("zipcode")
-    new_password = request.form.get("password")
-
-    conn = sql.connect("database.db")
-    cur = conn.cursor()
-
-    if new_password:
-        hashed_password = generate_password_hash(new_password)
-        cur.execute("""
-            UPDATE Users
-            SET BusinessName = ?, Street = ?, City = ?, State = ?, Zipcode = ?, Password = ?
-            WHERE Email = ?
-        """, (new_business_name, new_street, new_city, new_state, new_zipcode, hashed_password, email))
+    # Check user type to render the appropriate profile page
+    user_type = get_user_type(email)
+    
+    if user_type == 'B':
+        return render_template('buyersLandingPage.html', email=email)
+    elif user_type == 'S':
+        return render_template('sellersLandingPage.html', email=email)
+    elif user_type == 'H':
+        return render_template('helpdeskLandingPage.html', email=email)
     else:
-        cur.execute("""
-            UPDATE Users
-            SET BusinessName = ?, Street = ?, City = ?, State = ?, Zipcode = ?
-            WHERE Email = ?
-        """, (new_business_name, new_street, new_city, new_state, new_zipcode, email))
+        return redirect(url_for('index'))
 
-    conn.commit()
-    conn.close()
+# Route to edit the profile (this will allow users to edit their profile information)
+@profile_bp.route('/edit_profile', methods=['GET', 'POST'])
+def edit_profile():
+    email = request.args.get('email')  # Get the email of the logged-in user
 
-    # After editing profile, send back to the appropriate landing page
-    return redirect(url_for("landing_page", email=email))
+    if request.method == 'POST':
+        # Get the updated data from the form
+        updated_name = request.form['name']
+        updated_address = request.form['address']
+        updated_phone = request.form['phone']
+
+        # Update the user's information in the database
+        update_user_profile(email, updated_name, updated_address, updated_phone)
+
+        # Redirect to the profile page after successful update
+        return redirect(url_for('profile_bp.view_profile', email=email))
+
+    return render_template('editproduct.html', email=email)
+
+# Function to get the user type (Buyer, Seller, Helpdesk)
+def get_user_type(email):
+    result = ''
+    connection = sql.connect('database.db')
+    buyer = connection.execute('SELECT * FROM Buyers WHERE email = ?', (email,))
+    seller = connection.execute('SELECT * FROM Sellers WHERE email = ?', (email,))
+    helpdesk = connection.execute('SELECT * FROM HelpDesk WHERE email = ?', (email,))
+    if buyer.fetchone():
+        result += 'B'
+    if seller.fetchone():
+        result += 'S'
+    if helpdesk.fetchone():
+        result += 'H'
+    return result
+
+# Function to update the user's profile in the database
+def update_user_profile(email, name, address, phone):
+    connection = sql.connect('database.db')
+    cursor = connection.cursor()
+    
+    cursor.execute('''UPDATE Users SET name = ?, address = ?, phone = ? WHERE email = ?''', 
+                   (name, address, phone, email))
+    
+    connection.commit()
+    connection.close()
+
 
