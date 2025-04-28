792cad7b277cd7f46d8b4b81b08836057a14ee7b
nic-che <nichen2939@gmail.com>
2025-04-27 21:54:11 -0400

    reviews modal should be working

--- reviews.py
+++ reviews.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, url_for, redirect
 import sqlite3 as sql
+import random
+import os
+import pandas as pd
 
 reviews_bp = Blueprint('reviews', __name__, template_folder='templates')
 
@@ -7,14 +10,17 @@ reviews_bp = Blueprint('reviews', __name__, template_folder='templates')
 def reviews():
     rating = request.form.get('rateNumber')
     review = request.form.get('reviewInput')
-    order_id = request.form.get('listing_id')
 
-    print(f"Rating: {rating}")
-    print(f"Review: {review}")
-    print(f"Order ID (listing_id): {order_id}")
+    review_path = os.path.join('NittanyBusinessDataset_v3', 'Reviews.csv')
+    review_data = pd.read_csv(review_path)
+    existing_orders = set(review_data['Order_ID'])
+    while True:
+        order_id = random.randint(1, 9999)
+        if order_id not in existing_orders:
+            break
 
     connection = sql.connect('database.db')
     connection.execute('INSERT INTO Reviews (Order_ID, Rate, Review_Desc) VALUES (?, ?, ?)', (order_id, rating, review))
     connection.commit()
 
-    return render_template('productcatalogue.html')
+    return redirect(url_for('BrowseProducts.browse_products', currCategory="Root", email=request.form.get('email'), keyword="", keywordInput=""))
